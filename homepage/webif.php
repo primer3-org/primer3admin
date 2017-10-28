2ef49ed6c5c635b8fec5622c0dcb1b5d2307f08d
Andreas Untergasser <andreas@untergasser.de>
2017-10-27 23:32:36 -0700

    Descriptive link to web interface

--- homepage/webif.php
+++ homepage/webif.php
@@ -8,15 +8,15 @@ include("header.php")
 
 	<h2 id="original">Primer3Web</h2>
 		<p>Primer3Web is the original web-interface to Primer3.  Primer3Web was developed by Steve Rozen and Helen Skaletsky.  
-                   Primer3Web is maintained by Triinu Koressaar and Maido Remm..</p>  
+                   Primer3Web is maintained by Triinu Koressaar and Maido Remm.</p>  
 		<p>
-			<a href="http://primer3.ut.ee/">Primer3Web</a>
+			<a href="http://primer3.ut.ee/">Primer3Web at primer3.ut.ee</a>
 		</p>
 
 	<h2 id="primer3plus">Primer3Plus</h2>
 		<p>Primer3Plus is an updated, task-oriented web-interface to Primer3.  Primer3Plus is developed and maintained by Andreas Untergasser.</p>
 		<p>
-			<a href="http://primer3plus.com">Primer3Plus Web Interface</a>
+			<a href="http://primer3plus.com">Primer3Plus at primer3plus.com</a>
 		</p>
 </div> <!--end main div-->
 
