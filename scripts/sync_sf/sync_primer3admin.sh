dba96dae2426c79bdfd4ab114257ef1728646681
Andreas Untergasser <andreas@untergasser.de>
2022-01-26 11:04:22 +0100

    Add scripts admin and web

---
+++ scripts/sync_sf/sync_primer3admin.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./sync_one.sh primer3admin https://github.com/primer3-org/primer3admin.git https://git.code.sf.net/p/primer3/primer3admin
+
---
+++ scripts/sync_sf/sync_primer3web.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./sync_one.sh primer3web https://github.com/primer3-org/primer3web.git https://git.code.sf.net/p/primer3/primer3web
